4f6058e7867bf92d2be7db356fd4ced75df6e967
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-01-22 21:18:35 -0500

    Primeros cambios para configurar la validación del User-Agent

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorHandler');
 const validateApiKey = require('./middlewares/validateApiKey');
+const validateUserAgent = require('./middlewares/validateUserAgent'); 
 require('dotenv').config();
 
 const app = express();
@@ -10,11 +11,19 @@ const app = express();
 // Importando la rutas
 const exchangeRoutes = require('./routes/exchangeRoutes'); 
 
+// Configurar CORS
+const corsOptions = {
+  origin: ['https://mi-aplicacion-movil.com', 'http://localhost:3000'], // Dominios autorizados
+  methods: ['GET', 'POST'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'x-api-key'], // Encabezados permitidos
+};
+
 // Middlewares básicos
 app.use(helmet()); // Seguridad básica
-app.use(cors());   // Manejo de CORS
+app.use(cors(corsOptions));   // Manejo de CORS
 app.use(express.json()); // Parseo de JSON
 app.use(validateApiKey); // Validar clave API antes de las rutas
+app.use(validateUserAgent); // Validar User-Agent
 
 // Rutas
 app.use('/exchange', exchangeRoutes);
