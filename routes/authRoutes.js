808f9e44dbd22a46dc104ca576652cae7f2cf029
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-02-11 23:56:51 -0500

    Se unifica los callbacks de Google y Facebook

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,11 @@ const { login, register, refreshAccessToken, logout } = require('@controllers/au
 const { generateJWT } = require('@utils/generateJWT');
 const router = express.Router();
 
+const handleOAuthCallback = (req, res) => {
+    const token = generateJWT(req.user.id, req.user.role);
+    res.json({ accessToken: token });
+};
+
 // Rutas
 router.post('/register', register);
 router.post('/login', login);
@@ -13,18 +18,12 @@ router.post('/logout', logout);
 // Ruta para iniciar sesión con Google
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 // Callback de Google
-router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-    const token = generateJWT(req.user.id, req.user.role);
-    res.json({ accessToken: token });
-});
+router.get('/google/callback', passport.authenticate('google', { session: false }), handleOAuthCallback);
 
 // Ruta para iniciar sesión con Facebook
 router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
 // Callback de Facebook
-router.get('/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res) => {
-    const token = generateJWT(req.user.id, req.user.role);
-    res.json({ accessToken: token });
-});
+router.get('/facebook/callback', passport.authenticate('facebook', { session: false }), handleOAuthCallback);
 
 module.exports = router;
 
