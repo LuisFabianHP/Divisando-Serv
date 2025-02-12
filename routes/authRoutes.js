56c507b73ec4417b14d8101c65ac11d4a89b9656
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-02-11 23:38:14 -0500

    Estableciendo conexiones con Passport y estrategias OAuth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const passport = require('passport');
 const { login, register, refreshAccessToken, logout } = require('@controllers/authController');
+const { generateJWT } = require('@utils/generateJWT');
 const router = express.Router();
 
 // Rutas
@@ -7,6 +9,22 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh', refreshAccessToken);
 router.post('/logout', logout);
+
+// Ruta para iniciar sesión con Google
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// Callback de Google
+router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+    const token = generateJWT(req.user.id, req.user.role);
+    res.json({ accessToken: token });
+});
+
+// Ruta para iniciar sesión con Facebook
+router.get('/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+// Callback de Facebook
+router.get('/facebook/callback', passport.authenticate('facebook', { session: false }), (req, res) => {
+    const token = generateJWT(req.user.id, req.user.role);
+    res.json({ accessToken: token });
+});
 
 module.exports = router;
 
