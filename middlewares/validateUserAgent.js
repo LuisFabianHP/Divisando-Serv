e7705f1119e05a05337d5ef0ea4f9c3521846e00
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-02-12 17:05:33 -0500

    Se cambio la variable que contiene el user agent

--- middlewares/validateUserAgent.js
+++ middlewares/validateUserAgent.js
@@ -4,7 +4,7 @@ const validateUserAgent = (req, res, next) => {
   const userAgent = req.headers['user-agent'];
 
   // Lista de User-Agents permitidos, configurable desde .env
-  const allowedUserAgents = (process.env.ALLOWED_USER_AGENTS || 'MiAplicacionMovil/1.0').split(',');
+  const allowedUserAgents = (process.env.API_ALLOWED_USER_AGENTS || 'MiAplicacionMovil/1.0').split(',');
 
   if (!userAgent || !allowedUserAgents.includes(userAgent)) {
     const error = new Error('User-Agent no autorizado.');
